23d073a598bf5862b74169a9a495bc49cfd6ce6e
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2017-12-15 14:03:44 +0200

    adding output results to file

--- src/com/hackerrank/datastructures/Java1DArrayPart2CompareResults.java
+++ src/com/hackerrank/datastructures/Java1DArrayPart2CompareResults.java
@@ -1,6 +1,8 @@
 package com.hackerrank.datastructures;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -40,9 +42,23 @@ public class Java1DArrayPart2CompareResults {
 
 		String fileWithTestCases = "d://temp/tests.txt";
 		String fileWithResults = "d://temp/results.txt";
+
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+
 		try {
 			Scanner scanner = new Scanner(new File(fileWithTestCases));
 			Scanner scanner2 = new Scanner(new File(fileWithResults));
+
+			File fileOutput = new File("d://temp/comparisson.txt");
+
+			if (!fileOutput.exists()) {
+				fileOutput.createNewFile();
+			}
+
+			fw = new FileWriter(fileOutput.getAbsoluteFile());
+			bw = new BufferedWriter(fw);
+
 			int n = scanner.nextInt();
 			int errorCount = 0;
 			for (int i = 0; i < n; i++) {
@@ -54,27 +70,48 @@ public class Java1DArrayPart2CompareResults {
 					game[j] = scanner.nextInt();
 				}
 				String calculatedResult = (canWin(leap, game)) ? "YES" : "NO";
-				
+
 				if (!calculatedResult.equals(resultFromFile)) {
 					
-					int line = i + 1; 
-					System.out.println(line);
-					System.out.println("Dimension: " + game.length);
-					System.out.println("Leap: " + leap);
-					System.out.println(Arrays.toString(game));
-					System.out.println("Calculated vs From file");
-					System.out.println(calculatedResult + " vs " + resultFromFile);
-					System.out.println();
+					bw.write(Integer.toString(i + 1));
+					bw.newLine();
+					bw.write("Dimension: " + game.length);
+					bw.newLine();
+					bw.write("Leap: " + leap);
+					bw.newLine();
+					bw.write(Arrays.toString(game));
+					bw.newLine();
+					bw.write("Calculated vs From file");
+					bw.newLine();
+					bw.write(calculatedResult + " vs " + resultFromFile);
+					bw.newLine();
+					bw.newLine();
+
 					errorCount++;
 				}
 			}
-			System.out.println(errorCount);
+			bw.write("Total error count: " + Integer.toString(errorCount));
 			scanner.close();
 			scanner2.close();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
-		} 
+		} finally {
+
+            try {
+
+                if (bw != null)
+                    bw.close();
+
+                if (fw != null)
+                    fw.close();
+
+            } catch (IOException ex) {
+
+                ex.printStackTrace();
+
+            }
+        }
 	}
 	        
 }
