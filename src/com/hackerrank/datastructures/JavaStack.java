1386978a770f87ad495dad562db0ab13f8209224
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2017-12-18 18:43:07 +0200

    Java Stack

---
+++ src/com/hackerrank/datastructures/JavaStack.java
@@ -0,0 +1,19 @@
+package com.hackerrank.datastructures;
+
+import java.util.Scanner;
+
+public class JavaStack {
+
+    public static void main(String []argh)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String input=sc.next();
+
+            while(input.length() != (input = input.replaceAll("\\(\\)|\\[\\]|\\{\\}", "")).length());
+            System.out.println(input.isEmpty());
+        }
+
+    }
+}
